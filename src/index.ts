e85e799b15a4e4fac8bc6a7672b5ab5d721952c0
tasnim-rizvy <tasnimrizvy373@gmail.com>
2026-04-18 14:12:33 +0600

    feat: initialize server connections for MongoDB, PostgreSQL, and Redis in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import 'dotenv/config';
 import app from "./app";
+import { connectMongo } from './db/mongo';
+import { connectPostgres, runMigrations } from './db/postgres';
+import { connectRedis } from './db/redis';
 
 const PORT = process.env.PORT || 4000
 
 async function startServer() {
+    await connectMongo();
+    await connectPostgres();
+    await runMigrations();
+    await connectRedis();
+
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     })
